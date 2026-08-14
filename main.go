208ce6e501ef9bde3be31330f07cbfeca126ca38
PatrochR <rezacharsetad@outlook.com>
2026-08-14 17:56:46 +0330

    relax

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 	"strconv"
-
-	d "github.com/uaraven/delaunay/delaunay"
 
 	"github.com/gen2brain/raylib-go/raylib"
 	"github.com/pzsz/voronoi"
@@ -18,12 +17,14 @@ const (
 
 var inputDots int
 var dots []float64
-var actualPoints []voronoi.Vertex
-var triangles []d.Triangle
+var screenWidth int32
+var screenHeight int32
+var diagram *voronoi.Diagram
+var centroidedSites []voronoi.Vertex
 
 func main() {
-	screenWidth := int32(800)
-	screenHeight := int32(450)
+	screenWidth = int32(800)
+	screenHeight = int32(450)
 	rl.InitWindow(screenWidth, screenHeight, "voronoi")
 	setup()
 	rl.SetTargetFPS(60)
@@ -31,63 +32,70 @@ func main() {
 
 }
 
+var sites []voronoi.Vertex
+
 func setup() {
 	inputDotsS := os.Args[1]
 	inputDots, _ = strconv.Atoi(inputDotsS)
-
 	dots = initDots(inputDots)
-
-	p := dotsToPoints(dots)
-	actualPoints = dotsToPoints(dots)
-	delaunay := d.InitDelaunay(p)
-	triangles = delaunay.Triangulate()
+	sites = dotsToSites(dots)
 }
 
 func draw(screenWidth, screenHeight int32) {
-	// Create offscreen render target
-	target := rl.LoadRenderTexture(int32(screenWidth), int32(screenHeight))
-	defer rl.UnloadRenderTexture(target)
-
-	dirty := true // Flag to redraw texture when dots change
-
 	for !rl.WindowShouldClose() {
 		if rl.IsKeyPressed(rl.KeyEnter) {
 			dots = initDots(inputDots)
-			dirty = true
+			sites = dotsToSites(dots)
 		}
 
-		// Only redraw circles when dots change
-		if dirty {
-			rl.BeginTextureMode(target)
-			rl.ClearBackground(rl.RayWhite)
-			for _, point := range actualPoints {
-				rl.DrawCircle(int32(point.X), int32(point.Y), 3, rl.Black)
-				rl.DrawText(point.String(), int32(point.X)+5, int32(point.Y), 12, rl.DarkBlue)
+		bbox := voronoi.NewBBox(0, float64(screenWidth), 0, float64(screenHeight))
+		diagram := voronoi.ComputeDiagram(sites, bbox, true)
+
+		centroidedSites := make([]voronoi.Vertex, 0, len(sites))
+		for _, cell := range diagram.Cells {
+			poly := make([]voronoi.Vertex, 0, len(cell.Halfedges))
+			for _, he := range cell.Halfedges {
+				poly = append(poly, he.GetStartpoint())
 			}
-			for _, triangle := range triangles {
-				rl.DrawLine(int32(triangle.P1.X), int32(triangle.P1.Y), int32(triangle.P2.X), int32(triangle.P2.Y), rl.Green)
-				rl.DrawLine(int32(triangle.P2.X), int32(triangle.P2.Y), int32(triangle.P3.X), int32(triangle.P3.Y), rl.Green)
-				rl.DrawLine(int32(triangle.P3.X), int32(triangle.P3.Y), int32(triangle.P1.X), int32(triangle.P1.Y), rl.Green)
-				rl.DrawCircleLines(int32(triangle.Circumcenter.X), int32(triangle.Circumcenter.Y), float32(triangle.Circumradius), rl.Gray)
-				rl.DrawCircle(int32(triangle.Circumcenter.X), int32(triangle.Circumcenter.Y), 2, rl.Red)
-			}
-			rl.EndTextureMode()
-			dirty = false
+			centroidedSites = append(centroidedSites, centroid(poly))
 		}
 
-		// Per-frame: just draw one textured quad
+		dt := rl.GetFrameTime()
+		t := float32(1.0 - math.Exp(-1.0*float64(dt)))
+		for j := range sites {
+			pos := rl.Vector2Lerp(
+				rl.NewVector2(float32(sites[j].X), float32(sites[j].Y)),
+				rl.NewVector2(float32(centroidedSites[j].X), float32(centroidedSites[j].Y)),
+				t,
+			)
+			sites[j].X = float64(pos.X)
+			sites[j].Y = float64(pos.Y)
+		}
+
+		// 5. Draw DIRECTLY to the screen
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
-		// NOTE: RenderTexture's texture has flipped Y axis in raylib
-		rl.DrawTextureRec(
-			target.Texture,
-			rl.NewRectangle(0, 0, float32(screenWidth), -float32(screenHeight)),
-			rl.NewVector2(0, 0),
-			rl.White,
-		)
+		// Draw Voronoi lines
+		for _, cell := range diagram.Cells {
+			for _, he := range cell.Halfedges {
+				p := he.GetStartpoint()
+				p2 := he.GetEndpoint()
+				rl.DrawLine(int32(p.X), int32(p.Y), int32(p2.X), int32(p2.Y), rl.Red)
+			}
+		}
 
-		rl.DrawText(fmt.Sprintf("Dots: %d | FPS: %d", len(dots)/2, rl.GetFPS()), 10, 10, 20, rl.DarkGray)
+		// Draw actual points (Black)
+		for _, point := range sites {
+			rl.DrawCircle(int32(point.X), int32(point.Y), 3, rl.Black)
+		}
+
+		// Draw target centroids (Green)
+		for _, point := range centroidedSites {
+			rl.DrawCircle(int32(point.X), int32(point.Y), 3, rl.Green)
+		}
+
+		rl.DrawText(fmt.Sprintf("Dots: %d | FPS: %d", len(sites), rl.GetFPS()), 10, 10, 20, rl.DarkGray)
 		rl.EndDrawing()
 	}
 }
@@ -104,7 +112,7 @@ func initDots(len int) []float64 {
 	return dots
 }
 
-func dotsToPoints(dots []float64) []voronoi.Vertex {
+func dotsToSites(dots []float64) []voronoi.Vertex {
 	sites := make([]voronoi.Vertex, 0)
 	for i := 0; i < len(dots); i += 2 {
 		sites = append(sites, voronoi.Vertex{
@@ -114,4 +122,21 @@ func dotsToPoints(dots []float64) []voronoi.Vertex {
 	}
 
 	return sites
+}
+
+func centroid(poly []voronoi.Vertex) voronoi.Vertex {
+	var a, cx, cy float64
+	n := len(poly)
+	for i := 0; i < n; i++ {
+		p, q := poly[i], poly[(i+1)%n]
+		cross := p.X*q.Y - q.X*p.Y
+		a += cross
+		cx += (p.X + q.X) * cross
+		cy += (p.Y + q.Y) * cross
+	}
+	a *= 0.5
+	if math.Abs(a) < 1e-12 {
+		return poly[0]
+	}
+	return voronoi.Vertex{X: cx / (6 * a), Y: cy / (6 * a)}
 }
