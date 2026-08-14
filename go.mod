71e494666704dd18654178f099d899ba8988b35d
PatrochR <rezacharsetad@outlook.com>
2026-08-14 16:41:24 +0330

    before somthing stupied

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.4
 
 require (
 	github.com/gen2brain/raylib-go/raylib v0.60.0
+	github.com/pzsz/voronoi v0.0.0-20130609164533-4314be88c79f
 	github.com/uaraven/delaunay v0.0.0-20211007193209-66f180ed0688
 )
 
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	d "github.com/uaraven/delaunay/delaunay"
 
 	"github.com/gen2brain/raylib-go/raylib"
+	"github.com/pzsz/voronoi"
 )
 
 const (
@@ -16,30 +17,33 @@ const (
 )
 
 var inputDots int
+var dots []float64
+var actualPoints []voronoi.Vertex
+var triangles []d.Triangle
 
 func main() {
-	inputDotsS := os.Args[1]
-	inputDots, _ = strconv.Atoi(inputDotsS)
-	fmt.Println(inputDots)
-
 	screenWidth := int32(800)
 	screenHeight := int32(450)
-	dots := initDots(inputDots)
-	p := dotsToPoints(dots)
-	delaunay := d.InitDelaunay(p)
-	d.Delaunay
-
-	for _, x := range dots {
-		fmt.Print(x, " ")
-	}
-	fmt.Println("")
-
-	rl.InitWindow(screenWidth, screenHeight, "raylib [shapes] example - cubic-bezier lines")
-
-	// start := rl.NewVector2(0, 0)
-	// end := rl.NewVector2(float32(screenWidth), float32(screenHeight))
+	rl.InitWindow(screenWidth, screenHeight, "voronoi")
+	setup()
 	rl.SetTargetFPS(60)
+	draw(screenWidth, screenHeight)
 
+}
+
+func setup() {
+	inputDotsS := os.Args[1]
+	inputDots, _ = strconv.Atoi(inputDotsS)
+
+	dots = initDots(inputDots)
+
+	p := dotsToPoints(dots)
+	actualPoints = dotsToPoints(dots)
+	delaunay := d.InitDelaunay(p)
+	triangles = delaunay.Triangulate()
+}
+
+func draw(screenWidth, screenHeight int32) {
 	// Create offscreen render target
 	target := rl.LoadRenderTexture(int32(screenWidth), int32(screenHeight))
 	defer rl.UnloadRenderTexture(target)
@@ -56,8 +60,16 @@ func main() {
 		if dirty {
 			rl.BeginTextureMode(target)
 			rl.ClearBackground(rl.RayWhite)
-			for i := 0; i < len(dots); i += 2 {
-				rl.DrawCircle(int32(dots[i]), int32(dots[i+1]), 3, rl.Black)
+			for _, point := range actualPoints {
+				rl.DrawCircle(int32(point.X), int32(point.Y), 3, rl.Black)
+				rl.DrawText(point.String(), int32(point.X)+5, int32(point.Y), 12, rl.DarkBlue)
+			}
+			for _, triangle := range triangles {
+				rl.DrawLine(int32(triangle.P1.X), int32(triangle.P1.Y), int32(triangle.P2.X), int32(triangle.P2.Y), rl.Green)
+				rl.DrawLine(int32(triangle.P2.X), int32(triangle.P2.Y), int32(triangle.P3.X), int32(triangle.P3.Y), rl.Green)
+				rl.DrawLine(int32(triangle.P3.X), int32(triangle.P3.Y), int32(triangle.P1.X), int32(triangle.P1.Y), rl.Green)
+				rl.DrawCircleLines(int32(triangle.Circumcenter.X), int32(triangle.Circumcenter.Y), float32(triangle.Circumradius), rl.Gray)
+				rl.DrawCircle(int32(triangle.Circumcenter.X), int32(triangle.Circumcenter.Y), 2, rl.Red)
 			}
 			rl.EndTextureMode()
 			dirty = false
@@ -75,29 +87,31 @@ func main() {
 			rl.White,
 		)
 
-		rl.DrawText(fmt.Sprintf("Dots: %d | FPS: %.0f", len(dots)/2, rl.GetFPS()), 10, 10, 20, rl.DarkGray)
+		rl.DrawText(fmt.Sprintf("Dots: %d | FPS: %d", len(dots)/2, rl.GetFPS()), 10, 10, 20, rl.DarkGray)
 		rl.EndDrawing()
 	}
 }
 
-func initDots(len int) []int {
-	dots := make([]int, 0)
+func initDots(len int) []float64 {
+	dots := make([]float64, 0)
 	for range len {
 		x := rand.Intn(800)
 		y := rand.Intn(450)
 
-		dots = append(dots, x)
-		dots = append(dots, y)
+		dots = append(dots, float64(x))
+		dots = append(dots, float64(y))
 	}
 	return dots
 }
 
-func dotsToPoints(dots []int) []d.Point {
-	points := make([]d.Point, inputDots)
+func dotsToPoints(dots []float64) []voronoi.Vertex {
+	sites := make([]voronoi.Vertex, 0)
 	for i := 0; i < len(dots); i += 2 {
-		points = append(points, d.Point{
-			X: float64(dots[i]),
-			Y: float64(dots[i+1])})
+		sites = append(sites, voronoi.Vertex{
+			X: dots[i],
+			Y: dots[i+1],
+		})
 	}
-	return points
+
+	return sites
 }
